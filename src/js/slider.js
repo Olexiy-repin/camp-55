d2e0f8aa93eb20c827144ff377621a90fcdbbaa8
GoITAdmin <goitadmin@GoITAdmins-iMac-59.local>
2018-04-03 14:27:42 +0300

    finish adaptiv 1920px

--- src/js/slider.js
+++ src/js/slider.js
@@ -40,10 +40,45 @@
            margin: 10,
            dots: true
        },
-       1024:{
-           items:2,
-           nav:false,
-           margin: 5
+       1200:{
+           items:4,
+           nav:true,
+           margin: 30,
+           dots: true
+       }
+   }
+  });
+})();
+
+(() => {
+  $(".sec-14__slider").owlCarousel({
+   items : 1,
+   itemsCustom : true,
+
+   loop: true,
+
+   autoHeight : false,
+
+   margin : 5,
+   nav: true,
+   navText: ["<img src='../img/sec-06/larr.png'>","<img src='../img/sec-06/rarr.png'>"],
+   dots: false,
+
+   responsiveClass:true,
+   responsive:{
+       0:{
+           items:1,
+           nav:true
+       },
+       768:{
+           items:1,
+           nav:true,
+           margin: 10,
+       },
+       1200:{
+           items:1,
+           nav:true,
+           margin: 20
        }
    }
   });
